ae02408f104876e2faf01554bb4d76da2eb76f39
arron808 <arroncatapang84@gmail.com>
2025-09-19 11:11:03 +0800

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,31 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
-
-/*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
+    // Collapse the navbar when a link is clicked (for mobile)
+    const navbarCollapse = document.getElementById('navbarNav');
+    const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+    });
+    bsCollapse.hide();
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
+const scrollActive = () => {
     const scrollDown = window.scrollY
 
-  sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
+              sectionTop = current.offsetTop - 100, 
               sectionId = current.getAttribute('id'),
               sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
         
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
             sectionsClass.classList.add('active-link')
-        }else{
+        } else {
             sectionsClass.classList.remove('active-link')
         }                                                    
     })
@@ -48,8 +38,12 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
 });
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', {delay: 400}); 
+sr.reveal('.home__social-icon', { interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input', {interval: 200});
 
 ScrollReveal().reveal('.projects__img', {
   origin: 'bottom',
@@ -57,11 +51,5 @@ ScrollReveal().reveal('.projects__img', {
   duration: 1000,
   interval: 200,
   easing: 'ease-in-out',
+  
 });
-
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .contact__input',{interval: 200}); 
-
-
